39e5f7cd67da410b0648039ac2bdfcfffe82f5ce
Shashank pandey <126805929+HYDRO2070@users.noreply.github.com>
2024-10-09 06:32:12 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -21,6 +20,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+module.exports = app;
