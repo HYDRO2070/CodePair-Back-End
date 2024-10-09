4895d2f3278662491c0c6e92376fcf1d4e3619e6
Shashank pandey <126805929+HYDRO2070@users.noreply.github.com>
2024-10-09 06:39:48 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000; // Vercel uses 3000 by default
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
     .then(() => {
         console.log("MongoDb connected .....");
     })
